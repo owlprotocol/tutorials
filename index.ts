af72b05429a7705cb0fe40f3c2f23d8949cb14e2
Leo Vigna <leo.vigna@gmail.com>
2024-06-20 15:14:31 +0800

    update template

--- index.ts
+++ index.ts
@@ -1,67 +1,71 @@
+// Load environment variables
 import dotenv from "dotenv";
 dotenv.config();
 import { existsSync, writeFileSync } from "fs";
-// Permissionless imports
-import {
-  GetUserOperationReceiptReturnType,
-  UserOperation,
-  bundlerActions,
-  createSmartAccountClient,
-  getAccountNonce,
-  getSenderAddress,
-  getUserOperationHash,
-  waitForUserOperationReceipt,
-} from "permissionless";
-import {
-  privateKeyToSafeSmartAccount,
-  privateKeyToSimpleSmartAccount,
-  signerToSafeSmartAccount,
-} from "permissionless/accounts";
-import {
-  pimlicoBundlerActions,
-  pimlicoPaymasterActions,
-} from "permissionless/actions/pimlico";
-import {
-  createPimlicoBundlerClient,
-  createPimlicoPaymasterClient,
-} from "permissionless/clients/pimlico";
 // Viem imports
 import {
-  Address,
-  Hash,
-  Hex,
-  concat,
-  createClient as createViemClient,
-  createPublicClient,
-  encodeFunctionData,
-  http,
-  parseAbiItem,
+    Address,
+    Hash,
+    Hex,
+    concat,
+    createClient as createViemClient,
+    createPublicClient,
+    encodeFunctionData,
+    http,
+    parseAbiItem,
 } from "viem";
 import {
-  generatePrivateKey,
-  privateKeyToAccount,
-  signMessage,
+    generatePrivateKey,
+    privateKeyToAccount,
+    signMessage,
 } from "viem/accounts";
 import { lineaTestnet, polygonMumbai, sepolia } from "viem/chains";
+// Permissionless imports
+import {
+    GetUserOperationReceiptReturnType,
+    UserOperation,
+    bundlerActions,
+    createSmartAccountClient,
+    getAccountNonce,
+    getSenderAddress,
+    getUserOperationHash,
+    waitForUserOperationReceipt,
+} from "permissionless";
+import {
+    privateKeyToSafeSmartAccount,
+    privateKeyToSimpleSmartAccount,
+    signerToSafeSmartAccount,
+} from "permissionless/accounts";
+import {
+    pimlicoBundlerActions,
+    pimlicoPaymasterActions,
+} from "permissionless/actions/pimlico";
+import {
+    createPimlicoBundlerClient,
+    createPimlicoPaymasterClient,
+} from "permissionless/clients/pimlico";
+import {
+    ENTRYPOINT_ADDRESS_V07,
+    signUserOperationHashWithECDSA,
+} from "permissionless/utils";
 // OWL Protocol imports
 import { createClient } from "@owlprotocol/core-trpc/client";
-import {
-  ENTRYPOINT_ADDRESS_V07,
-  signUserOperationHashWithECDSA,
-} from "permissionless/utils";
 
-if (!existsSync(".env")) {
-  writeFileSync(".env", "API_KEY_SECRET=YOUR_API_KEY_SECRET");
-}
-
-const { API_KEY_SECRET } = process.env;
-
+//Hello World
 console.log("Welcome to Owl Protocol!");
 
-if (!API_KEY_SECRET) {
-  throw new Error(
-    "API_KEY_SECRET not found! Ensure it's correctly set in your .env file."
-  );
+//Create .env file if it doesn't exist
+if (!existsSync(".env")) {
+    console.debug(".env file did not exist so creating one right now with example envvars. Please add your API_KEY_SECRET to that file.")
+    writeFileSync(".env", "API_KEY_SECRET=YOUR_API_KEY_SECRET");
 }
 
-const client = createClient({ apiKey: API_KEY_SECRET });
+//Load API Key from .env file
+const { API_KEY_SECRET } = process.env;
+if (!API_KEY_SECRET || API_KEY_SECRET === "YOUR_API_KEY_SECRET") {
+    throw new Error(
+        `API_KEY_SECRET = ${API_KEY_SECRET}! Ensure it's correctly set in your .env file.`
+    );
+}
+
+// Add tutorial snippets below
