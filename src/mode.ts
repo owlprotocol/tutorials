c4c5632691acec92952c9d440a216dc424bf8fb7
Leo Vigna <leo.vigna@gmail.com>
2024-09-05 05:44:20 +0200

    update mode workshop

--- src/mode.ts
+++ src/mode.ts
@@ -5,7 +5,6 @@ dotenv.config();
 
 // Viem imports
 import {
-    Account,
     Address,
     Chain,
     createPublicClient,
@@ -33,7 +32,8 @@ import {
     getBundlerUrl,
     getPublicUrl,
 } from "@owlprotocol/clients";
-import { createClient } from "@owlprotocol/core-trpc";
+//TODO: Import MUST be from /client
+import { createClient } from "@owlprotocol/core-trpc/client";
 import { topupAddressL2 } from "@owlprotocol/viem-utils";
 import { publicActionsL2, walletActionsL1 } from "viem/op-stack";
 import { ENTRYPOINT_ADDRESS_V07_TYPE } from "permissionless/types";
@@ -60,7 +60,11 @@ if (!API_KEY_SECRET || API_KEY_SECRET === "YOUR_API_KEY_SECRET") {
 /***** Create Clients *****/
 
 // Initialize the Owl Protocol client with your API key
-const client = createClient({ apiKey: API_KEY_SECRET });
+const baseUrl = "http://localhost:3000/api";
+// const baseUrl = "https://api-staging.owl.build/api";
+const trpcUrl = `${baseUrl}/trpc`;
+
+const client = createClient({ apiKey: API_KEY_SECRET }, trpcUrl);
 
 /***** Create a user *****/
 //We use an external id to for idempotence
@@ -68,7 +72,7 @@ const user = await client.admin.user.Managed.create.mutate({ externalId: "my-use
 console.debug(user);
 
 //Run the tutorial in testnet / mainnet mode
-const environment: "testnet" | "mainnet" = "testnet";
+const environment: "testnet" | "mainnet" = "mainnet";
 const config = {
     testnet: {
         chainL1: sepolia,
@@ -94,11 +98,11 @@ const chainL1 = config[environment].chainL1;
 const chainL2 = config[environment].chainL2;
 
 const chainIdL1 = chainL1.chainId;
-const rpcL1 = getPublicUrl({ chainId: chainIdL1 }); //chainL1.rpcUrls.default.http[0];
+const rpcL1 = getPublicUrl({ chainId: chainIdL1, baseUrl }); //chainL1.rpcUrls.default.http[0];
 const blockExplorerL1 = chainL1.blockExplorers?.default.url!;
 
 const chainIdL2 = chainL2.chainId;
-const rpcL2 = getPublicUrl({ chainId: chainIdL2 }); //chainL2.rpcUrls.default.http[0]
+const rpcL2 = getPublicUrl({ chainId: chainIdL2, baseUrl }); //chainL2.rpcUrls.default.http[0]
 const blockExplorerL2 = chainL2.blockExplorers?.default.url!;
 
 // Create public viem client to read data from blockchain
@@ -117,22 +121,20 @@ console.debug({
 
 // Create paymaster viem client to sponsor UserOp
 // Learn more at https://docs.pimlico.io/permissionless/reference/clients/pimlicoPaymasterClient
-const paymasterClientL1 = createOwlPaymasterClient({ chainId: chainIdL1 });
-const paymasterClientL2 = createOwlPaymasterClient({ chainId: chainIdL2 });
+const paymasterClientL1 = createOwlPaymasterClient({ chainId: chainIdL1, baseUrl });
+const paymasterClientL2 = createOwlPaymasterClient({ chainId: chainIdL2, baseUrl });
 
 // Create bundler viem client to submit UserOp
 // Learn more at https://docs.pimlico.io/permissionless/reference/clients/bundlerClient
-const bundlerUrlL1 = getBundlerUrl({ chainId: chainIdL1 });
-const bundlerClientL1 = createOwlBundlerClient({ chainId: chainIdL1 });
-const bundlerUrlL2 = getBundlerUrl({ chainId: chainIdL2 });
-const bundlerClientL2 = createOwlBundlerClient({ chainId: chainIdL2 });
-
-console.debug({ bundlerUrlL1, bundlerUrlL2 });
+const bundlerUrlL1 = getBundlerUrl({ chainId: chainIdL1, baseUrl });
+const bundlerClientL1 = createOwlBundlerClient({ chainId: chainIdL1, baseUrl });
+const bundlerUrlL2 = getBundlerUrl({ chainId: chainIdL2, baseUrl });
+const bundlerClientL2 = createOwlBundlerClient({ chainId: chainIdL2, baseUrl });
 
 /***** Create Smart Wallet Owner *****/
 // Owner of the smart account
 const userId = user.userId;
-const owner = await createUserManagedAccount({ apiKey: API_KEY_SECRET, userId });
+const owner = await createUserManagedAccount({ apiKey: API_KEY_SECRET, userId, owlApiRestBaseUrl: baseUrl });
 console.debug(`Externally owned account address: ${blockExplorerL1}/address/${owner.address}`);
 console.debug(`Externally owned account address: ${blockExplorerL2}/address/${owner.address}`);
 
@@ -182,12 +184,28 @@ const smartAccountClientL2: SmartAccountClient<ENTRYPOINT_ADDRESS_V07_TYPE> = cr
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function bridgeEth() {
+async function bridgeEthTutorial({ amount }: { amount: bigint }) {
     const balanceL1Initial = await publicClientL1.getBalance({ address: smartAccountL1.address });
+    const balanceL2Initial = await publicClientL2.getBalance({ address: smartAccountL2.address });
+
+    if (balanceL2Initial > 0n) {
+        //bridgeEthTutorial was completed
+        console.log(
+            `${chainL1.name} ${smartAccountL1.address} ${formatEther(balanceL1Initial)} ${chainL1.nativeCurrency.name}`,
+        );
+        console.log(
+            `${chainL2.name} ${smartAccountL2.address} ${formatEther(balanceL2Initial)} ${chainL2.nativeCurrency.name}`,
+        );
+        return;
+    }
 
     //Bridge L1 funds to L2
     if (balanceL1Initial === 0n) {
-        throw new Error(`Please send 0.1 ETH to ${smartAccountL1.address} on ${chainL1.name} to start the tutorial`);
+        throw new Error(
+            `Please send ${formatEther(amount + parseEther("0.001"))} ETH to ${smartAccountL1.address} on ${
+                chainL1.name
+            } to start the tutorial`,
+        );
     }
 
     //Simple utility to bridge L1 -> L2 to target balance
@@ -202,7 +220,8 @@ async function bridgeEth() {
         walletClientL1: smartAccountClientL1,
         address: smartAccountL2.address,
         minBalance: 0n,
-        targetBalance: parseEther("0.05"),
+        targetBalance: amount,
+        l1Gas: null,
     });
 
     if (l1DepositReceipt) {
@@ -313,11 +332,43 @@ export async function getBridgeERC20L1toL2Transactions(params: BridgeERC20L1toL2
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function bridgeERC20() {
+async function bridgeUSDCTutorial({ amount }: { amount: bigint }) {
+    const l1Token = config[environment].USDC_L1;
+    const l2Token = config[environment].USDC_L2;
+
+    const balanceL1Initial = await publicClientL1.readContract({
+        address: l1Token,
+        abi: [balanceOf],
+        functionName: "balanceOf",
+        args: [smartAccountL1.address],
+    });
+    const balanceL2Initial = await publicClientL2.readContract({
+        address: l2Token,
+        abi: [balanceOf],
+        functionName: "balanceOf",
+        args: [smartAccountL2.address],
+    });
+
+    if (balanceL2Initial > 0n) {
+        //bridgeUSDCTutorial was completed
+        console.log(`${chainL1.name} ${smartAccountL1.address} ${balanceL1Initial / 1_000_000n} USDC`);
+        console.log(`${chainL2.name} ${smartAccountL2.address} ${balanceL2Initial / 1_000_000n} USDC`);
+        return;
+    }
+
+    //Bridge L1 USDC funds to L2
+    if (balanceL1Initial === 0n) {
+        throw new Error(
+            `Please send ${amount / 1_000_000n} USDC to ${smartAccountL1.address} on ${
+                chainL1.name
+            } to start the tutorial`,
+        );
+    }
+
     const { approval, bridge } = await getBridgeERC20L1toL2Transactions({
-        l1Token: config[environment].USDC_L1,
-        l2Token: config[environment].USDC_L2,
-        amount: 1_000_000n, //1 USDC
+        l1Token,
+        l2Token,
+        amount,
         from: smartAccountL1.address,
         /** Network params */
         publicClientL1,
@@ -330,10 +381,28 @@ async function bridgeERC20() {
     }
     transactions.push({ to: bridge.to, value: 0n, data: bridge.data });
 
-    //TODO: Update viem & pimlico versions for new interface
-    return smartAccountClientL1.sendTransactions({
+    //TODO: Update viem & pimlico versions for new interface (removes sendTransactions)
+    const hash = await smartAccountClientL1.sendTransactions({
         transactions,
     });
+
+    console.log(`${chainL1.name} -> ${chainL2.name} bridge USDC transaction ${blockExplorerL1}/tx/${hash}`);
+
+    //Updated L1 balance
+    const balanceL1 = await publicClientL1.readContract({
+        address: l1Token,
+        abi: [balanceOf],
+        functionName: "balanceOf",
+        args: [smartAccountL1.address],
+    });
+    const balanceL2 = await publicClientL2.readContract({
+        address: l2Token,
+        abi: [balanceOf],
+        functionName: "balanceOf",
+        args: [smartAccountL2.address],
+    });
+    console.log(`${chainL1.name} ${smartAccountL1.address} ${balanceL1 / 1_000_000n} USDC`);
+    console.log(`${chainL2.name} ${smartAccountL2.address} ${balanceL2 / 1_000_000n} USDC`);
 }
 
 export interface AlgebraSwapERC20Params {
@@ -407,12 +476,22 @@ export async function getSwapERC20Transactions(params: AlgebraSwapERC20Params): 
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function swapERC20() {
+async function swapERC20Tutorial({ amount }: { amount: bigint }) {
+    const balanceL2Initial = await publicClientL2.readContract({
+        address: config[environment].USDC_L2,
+        abi: [balanceOf],
+        functionName: "balanceOf",
+        args: [smartAccountL2.address],
+    });
+    if (balanceL2Initial < amount) {
+        throw new Error(`Please complete the bridgeUSDCTutorial first`);
+    }
+
     const { approval, swap } = await getSwapERC20Transactions({
         /** Swap params */
         tokenIn: config[environment].USDC_L2,
         tokenOut: config[environment].WETH_L2,
-        amountIn: 1_000_000n, //1 USDC;
+        amountIn: amount, //1 USDC;
         amountOutMinimum: 0n, //TODO: Get min out value
         from: smartAccountL2.address,
         /** Network params */
@@ -428,16 +507,23 @@ async function swapERC20() {
     transactions.push({ to: swap.to, value: 0n, data: swap.data });
 
     //TODO: Update viem & pimlico versions for new interface
-    return smartAccountClientL1.sendTransactions({
+    const hash = await smartAccountClientL2.sendTransactions({
         transactions,
     });
+
+    console.log(`${chainL2.name} USDC/WETH swap transaction ${blockExplorerL2}/tx/${hash}`);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 async function main() {
-    // bridgeEth();
-    // bridgeERC20();
-    // swapERC20();
+    //~2$ worth of Eth
+    // bridgeEthTutorial({ amount: parseEther("0.001") });
+
+    // 1 USDC
+    // bridgeUSDCTutorial({ amount: 1_000_000n });
+
+    // Swap USDC for ETH
+    swapERC20Tutorial({ amount: 1_000_000n });
 }
 
 await main();
