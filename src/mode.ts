849bafe04c20dd30ff1373e61b0d9de1611e581a
Leo Vigna <leo.vigna@gmail.com>
2024-09-11 20:28:15 +0200

    Feat/contracts algebra integral (#5)
    * updated tutorial with algebra integral sdk
    
    * refactor variable names

--- src/mode.ts
+++ src/mode.ts
@@ -14,6 +14,7 @@ import {
     PublicClient,
     encodeFunctionData,
     Hex,
+    formatUnits,
 } from "viem";
 
 // Permissionless imports
@@ -24,6 +25,11 @@ import { ENTRYPOINT_ADDRESS_V07 } from "permissionless/utils";
 
 // Owl Protocol imports
 import { balanceOf, allowance, approve } from "@owlprotocol/contracts-diamond/artifacts/IERC20";
+import {
+    balancePortfolio,
+    getERC20ApprovalTransaction,
+    getSwapExactInputTransaction,
+} from "@owlprotocol/contracts-algebra-integral";
 import { mainnet, sepolia, mode, modeTestnet } from "@owlprotocol/chains";
 import {
     createOwlBundlerClient,
@@ -32,13 +38,11 @@ import {
     getBundlerUrl,
     getPublicUrl,
 } from "@owlprotocol/clients";
-//TODO: Import MUST be from /client
 import { createClient } from "@owlprotocol/core-trpc/client";
 import { topupAddressL2 } from "@owlprotocol/viem-utils";
 import { publicActionsL2, walletActionsL1 } from "viem/op-stack";
 import { ENTRYPOINT_ADDRESS_V07_TYPE } from "permissionless/types";
 import { existsSync, writeFileSync } from "fs";
-import { SwapRouterAbi } from "./abis/SwapRouter.js";
 
 //Hello World
 console.log("Welcome to Owl Protocol!");
@@ -80,8 +84,13 @@ const config = {
         l1StandardBridge: "0xbC5C679879B2965296756CD959C3C739769995E2", //https://docs.mode.network/general-info/mainnet-contract-addresses/l1-l2-contracts
         USDC_L1: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238", //mint some at https://faucet.circle.com/
         USDC_L2: "0x514832A97F0b440567055A73fe03AA160017b990", //deployed using L2_OptimismMintableERC20Factory
+        MODE_L2: "0x0000000000000000000000000000000000000000", //unknown
         WETH_L2: "0x4200000000000000000000000000000000000006",
-        algrebraSwapRouter: null, //kim.exchange not deployed on testnet
+        //kim.exchange not deployed on testnet
+        poolInitCodeHash: "0x" as Hex,
+        poolDeployer: "0x0000000000000000000000000000000000000000",
+        swapRouter: "0x0000000000000000000000000000000000000000",
+        quoterV2: "0x0000000000000000000000000000000000000000",
     },
     mainnet: {
         chainL1: mainnet,
@@ -89,14 +98,31 @@ const config = {
         l1StandardBridge: "0x735aDBbE72226BD52e818E7181953f42E3b0FF21",
         USDC_L1: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
         USDC_L2: "0xd988097fb8612cc24eeC14542bC03424c656005f",
+        MODE_L2: "0xDfc7C877a950e49D2610114102175A06C2e3167a",
         WETH_L2: "0x4200000000000000000000000000000000000006",
-        algebraSwapRouter: "0xAc48FcF1049668B285f3dC72483DF5Ae2162f7e8",
+        poolInitCodeHash: "0xf96d2474815c32e070cd63233f06af5413efc5dcb430aee4ff18cc29007c562d" as Hex,
+        poolDeployer: "0x6414A461B19726410E52488d9D5ff33682701635",
+        quoterV2: "0x7c5aaa464f736740156fd69171505d344855d1e5",
+        swapRouter: "0xAc48FcF1049668B285f3dC72483DF5Ae2162f7e8",
     },
 } as const;
 
-const chainL1 = config[environment].chainL1;
-const chainL2 = config[environment].chainL2;
+// constants
+const {
+    chainL1,
+    chainL2,
+    l1StandardBridge,
+    USDC_L1,
+    USDC_L2,
+    MODE_L2,
+    WETH_L2,
+    poolInitCodeHash,
+    poolDeployer,
+    quoterV2,
+    swapRouter,
+} = config[environment];
 
+// clients
 const chainIdL1 = chainL1.chainId;
 const rpcL1 = getPublicUrl({ chainId: chainIdL1, baseUrl }); //chainL1.rpcUrls.default.http[0];
 const blockExplorerL1 = chainL1.blockExplorers?.default.url!;
@@ -331,8 +357,8 @@ export async function getBridgeERC20L1toL2Transactions(params: BridgeERC20L1toL2
 }
 
 export async function bridgeUSDCTutorial({ amount }: { amount: bigint }) {
-    const l1Token = config[environment].USDC_L1;
-    const l2Token = config[environment].USDC_L2;
+    const l1Token = USDC_L1;
+    const l2Token = USDC_L2;
 
     const balanceL1Initial = await publicClientL1.readContract({
         address: l1Token,
@@ -370,7 +396,7 @@ export async function bridgeUSDCTutorial({ amount }: { amount: bigint }) {
         from: smartAccountL1.address,
         /** Network params */
         publicClientL1,
-        l1StandardBridge: config[environment].l1StandardBridge,
+        l1StandardBridge,
     });
 
     const transactions: { to: Address; value: bigint; data: Hex }[] = [];
@@ -403,80 +429,9 @@ export async function bridgeUSDCTutorial({ amount }: { amount: bigint }) {
     console.log(`${chainL2.name} ${smartAccountL2.address} ${balanceL2 / 1_000_000n} USDC`);
 }
 
-export interface AlgebraSwapERC20Params {
-    /** Swap params */
-    tokenIn: Address;
-    tokenOut: Address;
-    amountIn: bigint;
-    amountOutMinimum: bigint;
-    from: Address;
-    recipient?: Address;
-    deadline?: bigint;
-    /** Network params */
-    publicClient: PublicClient;
-    algebraSwapRouter: Address;
-}
-/**
- * Get transactions to swap ERC20 token using Algebra Swap Router
- * @param params
- * @return necessary transactions to complete bridging
- */
-export async function getSwapERC20Transactions(params: AlgebraSwapERC20Params): Promise<{
-    approval?: { account: Address; to: Address; data: Hex };
-    swap: { account: Address; to: Address; data: Hex };
-}> {
-    const { tokenIn, tokenOut, amountIn, amountOutMinimum, algebraSwapRouter, from, publicClient } = params;
-    const recipient = params.recipient ?? from; //default swap to self
-    const deadline = params.deadline ?? (Date.now() + 600) * 1000; //default expire in 10min
-
-    // Check Allowance & Approve ERC20 for swap router
-    let approval: { account: Address; to: Address; data: Hex } | undefined;
-    const amountApproved = await publicClient.readContract({
-        address: tokenIn,
-        abi: [allowance],
-        functionName: "allowance",
-        args: [from, algebraSwapRouter],
-    });
-
-    if (amountApproved < amountIn) {
-        const data = await encodeFunctionData({
-            abi: [approve],
-            functionName: "approve",
-            args: [algebraSwapRouter, amountIn],
-        });
-        approval = { account: from, to: tokenIn, data };
-    }
-
-    const data = encodeFunctionData({
-        abi: SwapRouterAbi,
-        functionName: "exactInputSingle",
-        args: [
-            {
-                tokenIn,
-                tokenOut,
-                recipient,
-                deadline,
-                amountIn,
-                amountOutMinimum,
-                limitSqrtPrice: 0n,
-            },
-        ],
-    });
-
-    return {
-        approval,
-        swap: {
-            account: from,
-            to: algebraSwapRouter,
-            data,
-        },
-    };
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function swapERC20Tutorial({ amount }: { amount: bigint }) {
+export async function swapERC20Tutorial({ amount }: { amount: bigint }) {
     const balanceL2Initial = await publicClientL2.readContract({
-        address: config[environment].USDC_L2,
+        address: USDC_L2,
         abi: [balanceOf],
         functionName: "balanceOf",
         args: [smartAccountL2.address],
@@ -485,24 +440,27 @@ async function swapERC20Tutorial({ amount }: { amount: bigint }) {
         throw new Error(`Please complete the bridgeUSDCTutorial first`);
     }
 
-    const { approval, swap } = await getSwapERC20Transactions({
-        /** Swap params */
-        tokenIn: config[environment].USDC_L2,
-        tokenOut: config[environment].WETH_L2,
-        amountIn: amount, //1 USDC;
-        amountOutMinimum: 0n, //TODO: Get min out value
-        from: smartAccountL2.address,
-        /** Network params */
+    const { transaction: approval } = await getERC20ApprovalTransaction({
         publicClient: publicClientL2,
-        //@ts-expect-error
-        algebraSwapRouter: config[environment].algebraSwapRouter,
+        address: USDC_L2,
+        owner: smartAccountL2.address,
+        spender: swapRouter,
+        amount,
+    });
+    const swap = getSwapExactInputTransaction({
+        path: [USDC_L2, WETH_L2],
+        amountIn: amount,
+        amountOutMinimum: 0n,
+        recipient: smartAccountL2.address,
+        swapRouter,
+        weth: WETH_L2,
     });
 
     const transactions: { to: Address; value: bigint; data: Hex }[] = [];
     if (approval) {
-        transactions.push({ to: approval.to, value: 0n, data: approval.data });
+        transactions.push(approval);
     }
-    transactions.push({ to: swap.to, value: 0n, data: swap.data });
+    transactions.push(swap);
 
     //TODO: Update viem & pimlico versions for new interface
     const hash = await smartAccountClientL2.sendTransactions({
@@ -512,16 +470,52 @@ async function swapERC20Tutorial({ amount }: { amount: bigint }) {
     console.log(`${chainL2.name} USDC/WETH swap transaction ${blockExplorerL2}/tx/${hash}`);
 }
 
+export async function balancePortfolioTutorial() {
+    //TODO: bruteforce if some combinatiosn of weights break?
+    //Note: Slippage can impact success / failure of trade
+    const portfolio = await balancePortfolio({
+        publicClient: publicClientL2,
+        quoterV2,
+        poolInitCodeHash,
+        poolDeployer,
+        swapRouter,
+        account: smartAccountL2.address,
+        assets: [
+            { address: USDC_L2, weight: 1 },
+            { address: WETH_L2, weight: 0 },
+            { address: MODE_L2, weight: 0 },
+        ],
+        slippagePercent: 2,
+    });
+
+    const symbol = "ETH";
+    const units = 18;
+
+    console.debug(portfolio);
+    console.debug(`Total Holding Value ${symbol} ${formatUnits(portfolio.totalValue, units)}`);
+    console.debug(`Total Trade Value ${symbol} ${formatUnits(portfolio.totalTradeValue, units)}`);
+
+    return portfolio;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 async function main() {
     //~2$ worth of Eth
     // bridgeEthTutorial({ amount: parseEther("0.001") });
-
     // 1 USDC
     // bridgeUSDCTutorial({ amount: 1_000_000n });
-
     // Swap USDC for ETH
-    swapERC20Tutorial({ amount: 1_000_000n });
+    // swapERC20Tutorial({ amount: 1_000_000n });
+    // Balance portfolio
+    const portfolio = await balancePortfolioTutorial();
+
+    const hash = await smartAccountClientL2.sendTransactions({
+        transactions: portfolio.transactions,
+    });
+    console.debug({ hash });
+
+    const receipt = await publicClientL2.waitForTransactionReceipt({ hash });
+    console.debug({ receipt });
 }
 
 await main();
